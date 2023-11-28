fcb6e236dff697fb832e4b327f203d4e40cfa098
Pirajesh <pirajeshmr@gmail.com>
2023-11-28 23:35:54 +0530

    conflict resolve

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,20 +1,16 @@
 import { useEffect, useState } from "react";
 
 const useTimer = () => {
- const [timeLeft, setTimeLeft] = useState(300); 
+  const [timeLeft, setTimeLeft] = useState(300);
 
- useEffect(() => {
-   if (timeLeft <= 0) return;
-   const timeout = setTimeout(() => {
-     setTimeLeft(timeLeft - 1);
-   }, 1000);
-   return () => clearTimeout(timeout);
- }, [timeLeft]);
-
- const minutes = Math.floor(timeLeft / 60);
- const seconds = timeLeft % 60;
-
- return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+  useEffect(() => {
+    if (timeLeft <= 0) return;
+    const timeout = setTimeout(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [timeLeft]);
+    return timeLeft;
+  
 };
-
 export default useTimer;
