03d05c4c76d86e9fc7861029aed3e1855a99ffab
Pirajesh <pirajeshmr@gmail.com>
2024-02-03 01:56:45 +0530

    added responsiveness

--- frontend/src/components/Questionpage.js
+++ frontend/src/components/Questionpage.js
@@ -12,12 +12,11 @@ const Questionpage = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [resetHint, setResetHint] = useState(false);
 
-
-  const dateUnix=Date.now();
-  const date= new Date(dateUnix)
-  const hr = ('0' + date.getHours()).slice(-2);
-  const min = ('0' + date.getMinutes()).slice(-2); 
-  const sec = ('0' + date.getSeconds()).slice(-2); 
+  const dateUnix = Date.now();
+  const date = new Date(dateUnix);
+  const hr = ("0" + date.getHours()).slice(-2);
+  const min = ("0" + date.getMinutes()).slice(-2);
+  const sec = ("0" + date.getSeconds()).slice(-2);
   const curtime = `${hr}:${min}:${sec}`;
   // console.log(curtime);
 
@@ -49,47 +48,49 @@ const Questionpage = () => {
   const currentQuestionPercent =
     ((currentQuestionIndex + 1) / totalQuestionCount) * 100;
 
-  const handleOptionClick = (option,index) => {
-    const action= JSON.stringify(option);
+  const handleOptionClick = (option, index) => {
+    const action = JSON.stringify(option);
     // console.log(action);
     // console.log(index);
-    let va="A";
-    if(index==1){
-      va="B";
+    let va = "A";
+    if (index == 1) {
+      va = "B";
+    } else if (index == 2) {
+      va = "C";
+    } else if (index == 3) {
+      va = "D";
     }
-    else if(index==2){
-      va="C";
-    }
-    else if(index==3){
-      va="D";
-    }
-    const pageno=JSON.stringify(currentQuestionIndex+1);
-    const details={"user_id":"1","action":va,"page":pageno,"time":curtime}
+    const pageno = JSON.stringify(currentQuestionIndex + 1);
+    const details = { user_id: "1", action: va, page: pageno, time: curtime };
     // console.log(details);
-    axios.post('http://127.0.0.1:8080/api/unprompted/',details)
-    .then(response => {
-      console.log(response.data); 
-      setSelectedOption(option);
-    })
-    .catch(error => {
-      console.error('Error while making the Axios request:', error);
-    });
+    axios
+      .post("http://127.0.0.1:8080/api/unprompted/", details)
+      .then((response) => {
+        console.log(response.data);
+        setSelectedOption(option);
+      })
+      .catch((error) => {
+        console.error("Error while making the Axios request:", error);
+      });
     setSelectedOption(option);
-    
   };
-
-  
 
   const isContinueDisabled = !selectedOption || !question;
   const handleContinue = () => {
-    const pageno=JSON.stringify(currentQuestionIndex+1)
-    axios.post('http://127.0.0.1:8080/api/unprompted/',{"user_id":"1","action":"Continue","page":pageno,"time":curtime})
-    .then(response => {
-      console.log(response.data); 
-    })
-    .catch(error => {
-      console.error('Error while making the Axios request:', error);
-    });
+    const pageno = JSON.stringify(currentQuestionIndex + 1);
+    axios
+      .post("http://127.0.0.1:8080/api/unprompted/", {
+        user_id: "1",
+        action: "Continue",
+        page: pageno,
+        time: curtime,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error while making the Axios request:", error);
+      });
     if (!isContinueDisabled && question) {
       const nextQuestionIndex = currentQuestionIndex + 1;
       if (nextQuestionIndex < questions.length) {
@@ -101,9 +102,9 @@ const Questionpage = () => {
       }
     }
   };
-  
+
   const handleTimeOut = () => {
-      // console.log(currentQuestionIndex);
+    // console.log(currentQuestionIndex);
     const nextQuestionIndex = currentQuestionIndex + 1;
     // console.log(nextQuestionIndex);
     if (nextQuestionIndex < questions.length) {
@@ -116,38 +117,44 @@ const Questionpage = () => {
   };
 
   return (
-    <div className="h-screen w-screen divide-y divide-solid">
-      <div className="h-4/5 flex">
-        <div className="w-2/3 px-4">
+    <div className="h-screen w-screen sm:w-full divide-y divide-solid overflow-y-auto">
+      <div className="h-4/5 flex flex-col lg:flex-row overflow-y-auto">
+        <div className="w-full lg:w-2/3 px-4 overflow-y-auto max-h-[600px]">
           <div className="box-border p-4 text-lg text-blue-texts py-10">
             Q.{currentQuestionIndex + 1} {question && question.question}
           </div>
-          <div className="flex flex-col h-screen p-4 space-y-7">
+          <div className="flex flex-col p-4 space-y-7 ">
             {question &&
-              [question.op1, question.op2, question.op3, question.op4].map((option, index) => (
-                <Button
-                  key={index}
-                  className={`border border-blue-texts w-32 text-black p-4 rounded-lg ${
-                    selectedOption === option
-                      ? "bg-hover-color text-white"
-                      : "hover:bg-hover-color hover:text-white"
-                  }`}
-                  onClick={() => handleOptionClick(option,index)}
-                >
-                  {option}
-                </Button>
-              ))}
+              question.op1 &&
+              [question.op1, question.op2, question.op3, question.op4].map(
+                (option, index) => (
+                  <Button
+                    key={index}
+                    className={`border border-blue-texts w-32 text-black p-4 rounded-lg ${
+                      selectedOption === option
+                        ? "bg-hover-color text-white"
+                        : "hover:bg-hover-color hover:text-white"
+                    }`}
+                    onClick={() => handleOptionClick(option)}
+                  >
+                    {option}
+                  </Button>
+                )
+              )}
           </div>
         </div>
-        <div className="w-1/3 bg-blue-50">
-          <div className="h-10 bg-blue-texts w-fill flex justify-center text-white">
+        <div className="sm:w-full lg:w-1/3 bg-blue-50 lg:h-auto overflow-y-auto">
+          <div className="h-10 bg-blue-texts w-full flex justify-center text-white">
             AI Assistance
           </div>
           <div
-            className="bg-blue-50 p-4"
+            className="bg-blue-50 p-4 box sm:max-h-[calc(100vh - 210px)] sm:overflow-auto"
             style={{ whiteSpace: "break-spaces" }}
           >
-            <Chat text={question && question.misleading_suggestion} resetHint={resetHint} />
+            <Chat
+              text={question && question.misleading_suggestion}
+              resetHint={resetHint}
+            />
           </div>
         </div>
       </div>
@@ -164,7 +171,7 @@ const Questionpage = () => {
         </div>
         <div className="flex justify-center">
           <div className="text-blue-texts">
-            Time remaining 
+            Time remaining
             <Timer />
             {/* {Timer()==="0:0" && handleTimeOut()} */}
             {console.log(currentQuestionIndex)}
